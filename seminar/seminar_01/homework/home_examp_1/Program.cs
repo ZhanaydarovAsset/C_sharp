9b743cacd652fab277b8a207bd8ca93246d1d342
Zhanaydarov Asset <asset0889@mail.ru>
2023-02-11 15:50:44 +0600

    ДЗ выполнено

---
+++ seminar/seminar_01/homework/home_examp_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max " + a);
+    Console.WriteLine(" min " + b);
+}
+else
+{
+    Console.Write("max " + b);
+    Console.WriteLine(" min " + a);
+}
---
+++ seminar/seminar_01/homework/home_examp_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+int x, y, z;
+Console.WriteLine("Ввведите первое число: ");
+x = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввведите второе число: ");
+y = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввведите третье число: ");
+z = int.Parse(Console.ReadLine());
+
+if (x > y)
+{
+    if (x > z)
+    {
+        Console.WriteLine("максимальное число: " + x);
+    }
+    else
+    {
+        Console.WriteLine("максимальное число: " + z);
+    }
+}
+else
+{
+    if (y > z)
+    {
+        Console.WriteLine("максимальное число: " + y);
+    }
+    else
+    {
+        Console.WriteLine("максимальное число: " + z);
+    }
+}
