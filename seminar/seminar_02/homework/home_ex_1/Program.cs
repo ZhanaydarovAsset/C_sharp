ee2db4f61d3e86b2276847af7b95d2ec2893fcae
Zhanaydarov Asset <asset0889@mail.ru>
2023-02-17 01:50:04 +0600

    семинар 2 курс С_шарп

---
+++ seminar/seminar_02/homework/home_ex_1/Program.cs
@@ -0,0 +1,8 @@
+﻿int ShowSecondNumber(int number)
+{
+    int secondNumber = (number % 100) / 10;
+    return secondNumber;
+}
+int number = new Random().Next(100, 999);
+Console.WriteLine(number);
+Console.WriteLine(ShowSecondNumber(number));
