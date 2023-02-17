bc19f53a07781a3776ead16b71fce39a255fe30c
Zhanaydarov Asset <asset0889@mail.ru>
2023-02-17 11:52:23 +0600

    семинар 2 курс С_шарп ex_2

--- seminar/seminar_02/homework/home_ex_1/Program.cs
+++ seminar/seminar_02/homework/home_ex_1/Program.cs
@@ -1,8 +1,11 @@
-﻿int ShowSecondNumber(int number)
+﻿
+//принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int ShowSecondNumber(int number)
 {
     int secondNumber = (number % 100) / 10;
     return secondNumber;
-}
-int number = new Random().Next(100, 999);
+}int number = new Random().Next(100, 999);
 Console.WriteLine(number);
 Console.WriteLine(ShowSecondNumber(number));
+
---
+++ seminar/seminar_02/homework/home_ex_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+int ShowThird(int number)
+{
+
+    int thirdNumb = number % 10;
+    return thirdNumb;
+
+}
+int number = new Random().Next(10, 999);
+
+if (number >= 100 && number < 1000)
+{
+    Console.WriteLine(number);
+    Console.WriteLine(ShowThird(number));
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
