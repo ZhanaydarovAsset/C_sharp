44a0193fb675d24c30a79bc5a9cabd76947b033b
Zhanaydarov Asset <asset0889@mail.ru>
2023-02-18 12:18:06 +0600

    семинар 2 курс С_шарп ex_3

--- seminar/seminar_02/homework/home_ex_1/Program.cs
+++ seminar/seminar_02/homework/home_ex_1/Program.cs
@@ -5,7 +5,8 @@ int ShowSecondNumber(int number)
 {
     int secondNumber = (number % 100) / 10;
     return secondNumber;
-}int number = new Random().Next(100, 999);
+}
+int number = new Random().Next(100, 999);
 Console.WriteLine(number);
 Console.WriteLine(ShowSecondNumber(number));
 
---
+++ seminar/seminar_02/homework/home_ex_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string Holiday(int day)
+{
+    string weekend = "выходной";
+    string noWeekend = "не выходной";
+    if (day > 5)
+    {
+        return weekend;
+    }
+    return noWeekend;
+}
+
+int day = new Random().Next(1, 8);
+Console.WriteLine(day);
+Console.WriteLine(Holiday(day));
