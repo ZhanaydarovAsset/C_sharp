2ec70710799203f737fbb3b2b5874b3df12685e9
Zhanaydarov Asset <asset0889@mail.ru>
2023-02-22 01:57:58 +0500

    семинар 3 не доделал

---
+++ Lekcia/Lekcia_03/Example_1/Program.cs
@@ -0,0 +1,31 @@
+﻿void Method(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method("sms", 5);
+
+int Method2()
+{
+    return DateTime.Now.Year;
+}
+int god = Method2();
+//Console.WriteLine(god);
+
+
+string Method3(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method3(5, "f");
+Console.WriteLine(res);
---
+++ Lekcia/Lekcia_03/Example_3_sort/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] massif = { 11, 2, 8, 6, 1, 2, 9 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(massif);
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int minPos = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPos]) minPos = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+SelectionSort(massif);
+PrintArray(massif);
