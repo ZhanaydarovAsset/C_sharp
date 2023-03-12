fcfc2a54984fedb735cdff3a79022fad87d68fd4
Zhanaydarov Asset <asset0889@mail.ru>
2023-03-12 19:01:00 +0500

    семинар 8. задача 60 решено

---
+++ seminar/seminar_08/home_ex_56/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+using static Library.Shared;
+Console.Clear();
+int[] SummaLine(int[,] matr)
+{
+    int[] summLine = new int[matr.GetLength(0)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            summLine[i] += matr[i, j];
+        }
+    }
+    return summLine;
+}
+
+int size = InputIntUser("enter matrix size: ");
+int[,] matrix = new int[size, size];
+Fill2IntArray(matrix);
+Print2IntArray(matrix);
+
+int[] summ = SummaLine(matrix);
+Console.WriteLine(ArrayJoinToString(summ));
+int minOfSumm = summ.Min();
+int indexMin = Array.IndexOf(summ, minOfSumm);
+Console.WriteLine("строкa с наименьшей суммой элементов: "+ indexMin+1);
+
---
+++ seminar/seminar_08/home_ex_60/Program.cs
@@ -0,0 +1,58 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив,
+// добавляя индексы каждого элемента.
+using static Library.Shared;
+
+void Print3dArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                Console.Write($"{matr[i, j, k]} {(i, j, k)} ");
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int FindOf(int[,,] matr, int element)
+{
+    int x = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                if (matr[i, j, k] == element) { x = element; }
+
+            }
+        }
+
+    }
+    return x;
+}
+
+int[,,] matrix3d = new int[3, 3, 3];
+for (int i = 0; i < matrix3d.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix3d.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix3d.GetLength(2); k++)
+        {
+            matrix3d[i, j, k] = new Random().Next(10, 100);
+            while (matrix3d[i, j, k] != FindOf(matrix3d, matrix3d[i, j, k]))
+            {
+                matrix3d[i, j, k] = new Random().Next(10, 100);
+
+            }
+        }
+    }
+}
+
+
+Print3dArray(matrix3d);
+
